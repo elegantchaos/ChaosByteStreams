aecb4aeb3726d03f582fe3be121cf5dcc053855f
Sam Deane <sam@elegantchaos.com>
2024-08-31 11:53:04 +0100

    Added Pipe+AsyncBytes

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "ChaosByteStreams",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v15),
+    .watchOS(.v8),
+    .tvOS(.v15),
+  ],
+  products: [
+    .library(
+      name: "ChaosByteStreams",
+      targets: ["ChaosByteStreams"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "ChaosByteStreams",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "ChaosByteStreamsTests",
+      dependencies: ["ChaosByteStreams"]
+    )
+  ]
+)
