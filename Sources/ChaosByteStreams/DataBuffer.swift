e343aef718b7740c6eaf8a5cdbe8bab14e5435ac
Sam Deane <sam@elegantchaos.com>
2024-09-06 16:08:35 +0100

    fixed unit tests

--- Sources/ChaosByteStreams/DataBuffer.swift
+++ Sources/ChaosByteStreams/DataBuffer.swift
@@ -8,18 +8,20 @@ import Foundation
 public actor DataBuffer {
   var data = Data()
   var continuations: [AsyncStream<UInt8>.Continuation] = []
-
+  
   /// Append data to the buffer.
   /// We'll notify all continuations that new data is available.
   func append(_ bytes: Data) {
-    print("append")
+    print("DataBuffer appending")
     assert(!bytes.isEmpty)
     data.append(bytes)
+    print("DataBuffer appended")
     for continuation in continuations {
-      for byte in bytes { continuation.yield(byte) }
+      print("DataBuffer sending new bytes to continuation")
+      DataBuffer.sendBytes(bytes, to: continuation)
     }
   }
-
+  
   /// Finish the buffer.
   /// This means that no more data will be added to the buffer.
   /// We'll notify all continuations that the buffer is done.
@@ -28,58 +30,76 @@ public actor DataBuffer {
     for continuation in continuations { continuation.finish() }
     continuations.removeAll()
   }
-
+  
   /// Add a continuation to the buffer.
   /// We immediately yield all current data in the buffer to the continuation.
   /// When new data is available, we'll yield it to the continuation.
-  func registerContinuation(_ continuation: AsyncStream<UInt8>.Continuation) async {
-    print("register")
-    continuations.append(continuation)
-    if !data.isEmpty {
-      let d = data
-      print("sending existing data")
-      for byte in d {
-        print(String(format: "%c", byte))
-        continuation.yield(byte)
+  ///
+  nonisolated func registerContinuation(_ continuation: AsyncStream<UInt8>.Continuation) {
+    Task.detached { [weak self] in
+      await self?._registerContinuation(continuation)
+      if let data = await self?.data {
+        print("continuation sending existing data")
+        DataBuffer.sendBytes(data, to: continuation)
       }
-      print("sent")
     }
   }
-
-  func removeContinuation(_ continuation: AsyncStream<UInt8>.Continuation) {
+  
+  nonisolated static func sendBytes(_ data: Data, to continuation: AsyncStream<UInt8>.Continuation) {
+    for byte in data {
+      print(String(format: "%c", byte))
+      continuation.yield(byte)
+    }
+  }
+  
+  func _registerContinuation(_ continuation: AsyncStream<UInt8>.Continuation) async {
+    print("continuation appending")
+    continuations.append(continuation)
+    print("continuation appended")
+  }
+  
+  nonisolated func removeContinuation(_ continuation: AsyncStream<UInt8>.Continuation) {
     print("remove")
   }
-
+  
   /// Return a byte sequence that reads from this buffer.
   func makeBytes() -> AsyncBytes { AsyncBytes(buffer: self) }
-
+  
   /// A byte sequence that is empty.
   static var noBytes: AsyncBytes { AsyncBytes(buffer: nil) }
-
+  
   /// A byte sequence that reads from a buffer.
   public struct AsyncBytes: AsyncSequence, Sendable {
     public typealias Element = UInt8
-
+    
     /// Buffer we're reading from.
     let buffer: DataBuffer?
-
+    
     /// Make an iterator that reads data from the pipe's file handle
     /// and outputs it as a byte sequence.
     public func makeAsyncIterator() -> AsyncStream<Element>.Iterator {
-      print("make")
-      return AsyncStream { continuation in
+      print("DataBuffer making iterator")
+      return makeStream().makeAsyncIterator()
+    }
+    
+    public func makeStream() -> AsyncStream<UInt8> {
+      print("DataBuffer making stream")
+      let s = AsyncStream<UInt8> { continuation in
+        print("DataBuffer got continuation")
         guard let buffer else {
           continuation.finish()
           return
         }
         continuation.onTermination = { termination in
-          Task { await buffer.removeContinuation(continuation) }
+          print("DataBuffer continuation terminated")
+          buffer.removeContinuation(continuation)
         }
-        Task.detached {
-          await buffer.registerContinuation(continuation)
-          print("registered")
-        }
-      }.makeAsyncIterator()
+        print("DataBuffer registering")
+        buffer.registerContinuation(continuation)
+        print("DataBuffer registered")
+      }
+      print("DataBuffer made stream")
+      return s
     }
   }
 }
--- Tests/ChaosByteStreamsTests/ChaosByteStreamsTests.swift
+++ Tests/ChaosByteStreamsTests/ChaosByteStreamsTests.swift
@@ -8,25 +8,26 @@ let testBytes = testLines.joined(separator: "\n").data(using: .utf8)!
 
 func makeTestBuffer() async -> DataBuffer {
   let buffer = DataBuffer()
-  // await buffer.append(testBytes)
+  DispatchQueue.global(qos: .background).async {
+    Task {
+      await buffer.append(testBytes)
+      try? await Task.sleep(nanoseconds: 100)
+      await buffer.finish()
+    }
+  }
   return buffer
 }
 
 @Test func testBufferByteStream() async throws {
   let buffer = await makeTestBuffer()
-  await buffer.append(testBytes)
   var expected = testLines
-  print("a")
-  // print(await buffer.data)
   let input = await buffer.makeBytes()
-  print("a")
+  
+  print("reading from input")
   for await l in input.lines {
     #expect(l == expected.first)
     expected.removeFirst()
   }
-  print("a")
-  await buffer.finish()
-  print("a")
 }
 
 @Test func testByteStreamToData() async throws {
